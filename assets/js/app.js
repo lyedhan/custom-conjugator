9291ebd339b71d63c25a7a9c8492575765bc6a18
Your Name <you@example.com>
2026-05-03 19:20:13 +0100

    UI adjustmenets, start on Settings UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,3 @@ import { router } from "./router.js";
 
 window.addEventListener("hashchange", router);
 router();
-
-let json = {};
-console.log(json);
-
-json["person"] = {};
-console.log(json);
-
-json["person"]["type"] = {};
-console.log(json);
